c6f463fddbaf77e61dffb5bc551f4246db7ae117
Johann3421 <1355430@senati.pe>
2023-08-17 09:54:10 -0500

    Creando la seccion para insertar numeros

--- Hello_world/src/main/java/com/mycompany/hello_world/Hello_world.java
+++ Hello_world/src/main/java/com/mycompany/hello_world/Hello_world.java
@@ -15,5 +15,18 @@ public class Hello_world {
         System.out.println("Mi primera calculadora");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Calculadora basica");
+        System.out.println("Operaciones disponibles");
+        System.out.println("1: suma");
+        System.out.println("2: resta");
+        
+        System.out.println("Selecciona una operacion: (1/2)");
+        int opcion = scanner.nextInt();
+        System.out.println("Ingrese el primer numero: ");
+        double numero1= scanner.nextDouble();
+        System.out.println("Ingrese el Segundo Numero: ");
+        double numero2= scanner.nextDouble();
+        double resultado=0.0;
+        
+    
     }
 }
