2e6450029a09e5967f5a622b09432e7bf43cfd37
Johann3421 <1355430@senati.pe>
2023-08-17 09:37:11 -0500

    Probando comentario

---
+++ Hello_world/src/main/java/com/mycompany/hello_world/Hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello_world;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
